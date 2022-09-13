d8832e260391064ce9cce056dbeabb1b0292c60d
markcaf <marcocafassog@gmail.com>
2022-09-13 23:16:16 +0200

    feat(comics): add index structure with main layout

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title', 'Main comics')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>
+                    Main content
+                </h2>
+            </div>
+        </div>
+    </div>
+@endsection
