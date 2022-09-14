e64cfe9bc0988542dcc29dd73a9cdedfa12d3f5a
markcaf <marcocafassog@gmail.com>
2022-09-14 02:06:35 +0200

    feat: add home.blade as homepage

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', 'Main comics')
+
+@section('main-content')
+    <div class="container my-5 py-2">
+        <div class="row">
+            <div class="col-12">
+                <h2>Admin Homepage</h2>
+                <h5>Welcome to the admin homepage, use the upper navbar to navigate</h5>
+            </div>
+        </div>
+    </div>
+@endsection
