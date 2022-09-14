e726bf1cccac66198c957e412b4a3e00649a583d
markcaf <marcocafassog@gmail.com>
2022-09-14 18:59:04 +0200

    refactor: update web.php with route resource

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('admin.home');
-Route::get('/comics', 'ComicController@index')->name('comics.index');
+Route::resource('/comics', 'ComicController');
