dafe60fbc252e6b8e9601f11683da692a4fab0c3
markcaf <marcocafassog@gmail.com>
2022-09-14 02:07:16 +0200

    refactor: update menu links with home and comics

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand me-5" href="{{ route('comics.index') }}">LARAVEL | COMICS</a>
+            <a class="navbar-brand me-5" href="{{ route('admin.home') }}">LARAVEL | COMICS</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -9,10 +9,10 @@
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('comics.index') }}">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('admin.home') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Comics</a>
+                        <a class="nav-link" href="{{ route('comics.index') }}">Comics</a>
                     </li>
                 </ul>
                 <span class="navbar-text">
