5974599eda2f77538dec5fdb62284f00117ace8f
markcaf <marcocafassog@gmail.com>
2022-09-16 02:51:13 +0200

    feat(controller): add edit function

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -75,9 +75,11 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($slug)
     {
-        //
+        $comic = Comic::where('slug', $slug)->first();
+        
+        return view('comics.edit', compact('comic'));
     }
 
     /**
