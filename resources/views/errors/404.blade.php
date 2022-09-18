245736ea86d9f19e34cec09239b1a3e18f65e48f
markcaf <marcocafassog@gmail.com>
2022-09-19 01:51:08 +0200

    feat: create new 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', '404 Not Found')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="https://tinypic.host/images/2022/09/19/404-marvel.jpg" alt="404 Not Found" class="w-100 h-100">
+            </div>
+        </div>
+    </div>
+@endsection
