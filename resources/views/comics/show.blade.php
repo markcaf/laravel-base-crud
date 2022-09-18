61408edd85af3fed18be110d30c53a1af6660d52
markcaf <marcocafassog@gmail.com>
2022-09-18 18:23:35 +0200

    feat(show): add edit and delete buttons

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -18,7 +18,16 @@
                       <h4 class="card-title">{{ $comic->title }}</h4>
                       <p class="card-text px-5">{{ $comic->description }}</p>
                       <h5 class="card-text">${{ $comic->price }}</h5>
-                      <a href="{{ route('comics.index') }}" class="btn btn-danger my-4">Back to Comics list</a>
+                      <div class="d-flex justify-content-center gap-4">
+                        <a href="{{ route('comics.edit', $comic->slug) }}" class="btn btn-success my-4">Edit this comic</a>
+
+                        <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger my-4">Remove this comic</button>
+                        </form>
+
+                      </div>
                     </div>
                     <div class="card-footer text-white">
                         Sale date: {{ $comic->sale_date }}
